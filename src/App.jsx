58b97fb7f28d142f1ed87c93c0542f1d76b3a267
mayank12gt <mayank.gt15@gmail.com>
2023-12-07 17:34:07 +0530

    Added error handling, loading indicator

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from 'react'
 
-import {Box, Button, Card, CardFooter, CardHeader, CardBody, CircularProgress, Flex, Heading,IconButton,Input,InputGroup,InputRightElement,MenuIcon,Spacer,Text, VStack, Image, Stack, Divider, ButtonGroup, Avatar } from '@chakra-ui/react'
+import {Alert,AlertIcon,Badge,Box, Button, Card, CardFooter, CardHeader, CardBody, CircularProgress, Flex, Heading,IconButton,Input,InputGroup,InputRightElement,MenuIcon,Spacer,Text, VStack, Image, Stack, Divider, ButtonGroup, Avatar,AlertTitle, AlertDescription } from '@chakra-ui/react'
 import {FaSearch} from 'react-icons/fa'
 import axios from 'axios'
 import ProfileCard from './components/ProfileCard'
@@ -14,7 +14,8 @@ function App() {
   const [username, setUsername] = useState('')
   const [profile, setProfile] = useState('')
   const [repos, setRepos] = useState([])
-
+  const [err, setErr] = useState(null)
+  const [loading, setLoading] = useState(false)
 useEffect(() => {
 getUserData()
 }, [username])
@@ -25,24 +26,53 @@ useEffect(() => {
  
 
 const getUserData = ()=>{
-  if(username!==''){
-    
-   
-  axios.get(`https://api.github.com/users/${username}`)
-  .then((res)=>{
-    console.log(res.data)
-    setProfile(res.data)
-    axios.get(`https://api.github.com/users/${username}/repos`).
-    then((res)=>{
-      console.log(res.data)
-      setRepos(res.data)
-    }).catch((err)=>{console.log('no repos found')})
-  }
-    )
-  .catch((err)=>{console.log('user not found')})
-  } 
-  else{
-    
+  const searchVal = contentRef.current.value
+  if(searchVal!==''){
+//   axios.get(`https://api.github.com/users/${username}`)
+//   .then((res)=>{
+//     console.log(res.data)
+//     setErr(null)
+//     setProfile(res.data)
+//     axios.get(`https://api.github.com/users/${username}/repos`).
+//     then((res)=>{
+//       console.log(res.data)
+//       setRepos(res.data)
+//     }).catch((err)=>{console.log('no repos found')})
+//   }
+//     )
+//   .catch((err)=>{console.log('user not found')
+//   setProfile(null)
+//   setRepos(null)
+// setErr('User not found. Please check the username.')
+// })
+
+setProfile(null)
+setRepos(null)
+setErr(null)
+setLoading(true)
+
+const userReq = axios.get(`https://api.github.com/users/${searchVal}`)
+const userRepoReq = axios.get(`https://api.github.com/users/${searchVal}/repos`)
+
+Promise.all([userReq, userRepoReq]).
+then((res)=>{
+  console.log(res[0].data)
+  console.log(res[1].data)
+  setLoading(false)
+  setProfile(res[0].data)
+  setRepos(res[1].data)
+  setErr(null)
+}).
+catch((err)=>{
+  console.log(err)
+  setLoading(false)
+  setProfile(null)
+  setRepos(null)
+  setErr(err)
+})
+} 
+else{
+    setErr(null)
     setUsername('')
     setProfile('')
     setRepos([])
@@ -51,13 +81,14 @@ const getUserData = ()=>{
 
 
 const getRepos = ()=>{
-  setUsername(contentRef.current.value)
+  //setUsername(contentRef.current.value)
   console.log(contentRef.current.value)
   console.log(username)
+  getUserData()
 }
   return (
 
-    <VStack alignItems={'center'}>
+    <VStack h={'100vh'}  alignItems={'center'}>
       <Flex
       bgColor={'gray.100'}
       p={4}
@@ -73,8 +104,7 @@ const getRepos = ()=>{
       >Git Search</Heading>
   
     </Flex>
-
-      <InputGroup  
+    <InputGroup  
       variant={'filled'}
       w={'80%'}
       maxW={'450px'}
@@ -99,9 +129,17 @@ const getRepos = ()=>{
     </InputRightElement>
     </InputGroup>
 
-   
+    {loading && <CircularProgress   margin={'auto'}   isIndeterminate color='blue.500' />}
+
+   {err&& <Alert  mt={4}  rounded={'md'} w={'80%'} maxW={'450px'} status='error'>
+    <AlertIcon />
+    <Box pl={4}>
+    <AlertTitle>{err.code}</AlertTitle>
+  <AlertDescription   >{err.message}</AlertDescription>
+  </Box>
+  </Alert>}
+
     {profile&&<ProfileCard  profile={profile}/>}
-   
    
     {repos && repos.map(repo=>
      
